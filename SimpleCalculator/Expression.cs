6ce746cafeb802328697078f4e2bcfab22b89869
Andi DeVois <adevois@gmail.com>
2016-08-20 09:01:29 -0700

    expression pieces parsing

--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -10,13 +10,21 @@ namespace SimpleCalculator
     public class Expression
     {
         string pattern = @"^(?<NumOne>-?\d+)\s?(?<Oprtr>[\+\-\*\/%])\s?(?<NumTwo>-?\d+)";
+        public int First { get; set; }
+
+        public int Second;
+        public string Operator = "";
 
         public void Splitter(string UserInput)
         {
             Regex Regex = new Regex(pattern);
+            Match Matches = Regex.Match(UserInput);
 
             if (true == Regex.IsMatch(UserInput))
             {
+                First = Convert.ToInt32(Matches.Groups["NumOne"].Value);
+                Second = Convert.ToInt32(Matches.Groups["NumTwo"].Value);
+                Operator = Matches.Groups["Oprtr"].Value;
 
             }
 
